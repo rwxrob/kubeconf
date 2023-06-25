12a336a54b0bbb871d6eeeb9861f6712eef309b0
rwxrob <rob@rwx.gg>
2023-06-25 18:39:55 -0400

    fix: remove api.Config encapsulation

--- kubeconf.go
+++ kubeconf.go
@@ -1,41 +1,16 @@
 package kubeconf
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-
 	cc "k8s.io/client-go/tools/clientcmd"
-	api "k8s.io/client-go/tools/clientcmd/api"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
-// Config wraps the client-go type of the same name to allow for
-// additional marshalling methods. See
-// k8s.io/client-go/tools/clientcmd/api for more details.
-type Config api.Config
-
-// Load returns a Config struct created using the default loading rules
-// for the clientcmd/api (the same used by kubectl itself). See kubectl
-// config --help for more information.
-func Load() (Config, error) {
-	conf, err := cc.NewNonInteractiveDeferredLoadingClientConfig(
+// Load returns an api.Config struct created using the default loading rules
+// for the clientcmd/api (the same as kubectl itself). See kubectl
+// config --help for more information. This is useful for viewing how
+// kubectl actually resolved loading multiple configurations from
+// different files.
+func Load() (api.Config, error) {
+	return cc.NewNonInteractiveDeferredLoadingClientConfig(
 		cc.NewDefaultClientConfigLoadingRules(), nil).RawConfig()
-	return Config(conf), err
 }
-
-// JSON outputs the Context as JSON (null if <nil>). Errors during
-// unmashalling are logged and null returned.
-func (c *Config) JSON() string {
-	if c == nil {
-		return `null`
-	}
-	byt, err := json.Marshal(c)
-	if err != nil {
-		log.Print(err)
-		return `null`
-	}
-	return string(byt)
-}
-
-func (c Config) String() string { return c.JSON() }
-func (c *Config) Print()        { fmt.Println(c) }
